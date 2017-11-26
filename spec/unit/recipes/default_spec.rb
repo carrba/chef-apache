55f558b6b3da2c0f8c2a1f89764e5d5b2f53c30b
carrb <carrb@chefwork.itbc.dev>
2017-11-26 20:01:14 +0000

    small changes to kitchen.yml and default_spec.rv

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,20 +7,26 @@
 require 'spec_helper'
 
 describe 'test::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
+  context 'Rnning against Centos 7.2.1511' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+      expect{ chef_run }.to_not raise_error
     end
 
-    it 'installs Apache2' do
-      expect(chef_run).to install_package('Apache2')
+    it 'installs httpd' do
+      expect(chef_run).to install_package('httpd')
+    end
+    it 'enables httpd' do
+      expect(chef_run).to enable_service('httpd')
+    end
+    it 'starts http' do
+      expect(chef_run).to start_service('httpd')
     end
   end 
 end
